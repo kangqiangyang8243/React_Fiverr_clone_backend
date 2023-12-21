6e32cadc18e19f2ffa9993533ffc385aaf2a4952
Kqyang29 <yangkq0417@163.com>
2023-12-20 23:09:32 -0800

    'fixf'

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const Users = require("../models/userModels");
 const jwt = require("jsonwebtoken");
 
